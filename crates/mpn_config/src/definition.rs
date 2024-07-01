e425d8231c95c8c0467b8fe1ba8881bb7af26239
Shixin Huang <shixinhuang99@gmail.com>
2024-07-02 00:52:17 +0800

    WIP

--- crates/mpn_config/src/definition.rs
+++ crates/mpn_config/src/definition.rs
@@ -1,4 +1,19 @@
+use std::sync::OnceLock;
+
+use unindent::unindent;
+
 use crate::type_def::TypeDef;
+
+pub struct DefinitionOptions {
+	key: &'static str,
+	default: TypeDef,
+	types: &'static [TypeDef],
+	description: &'static str,
+	multiple: bool,
+	short: Option<&'static [&'static str]>,
+	deprecated: Option<&'static str>,
+	exclusive: Option<&'static [&'static str]>,
+}
 
 pub struct Definition {
 	key: &'static str,
@@ -12,7 +27,9 @@ pub struct Definition {
 	hint: String,
 	short: Option<&'static [&'static str]>,
 	usage: String,
-	// deprecated: &'static str,
+	deprecated: Option<&'static str>,
+	exclusive: Option<&'static [&'static str]>,
+	// flatten
 }
 
 impl Definition {
@@ -35,13 +52,56 @@ impl Definition {
 			types: options.types,
 			description: options.description,
 			multiple: options.multiple,
-			env_export: false,
+			env_export: true,
 			short: None,
 			default_description,
 			type_description,
 			hint,
 			usage,
+			deprecated: options.deprecated,
+			exclusive: options.exclusive,
 		}
+	}
+
+	pub fn describe(&self) -> String {
+		let description = unindent(self.description);
+		let no_env_export = if self.env_export {
+			""
+		} else {
+			"\nThis value is not exported to the environment for child processes.\n"
+		};
+		let deprecated = if let Some(v) = self.deprecated {
+			format!("* DEPRECATED: {}\n", unindent(v))
+		} else {
+			"".to_string()
+		};
+		let exclusive = if let Some(v) = self.exclusive {
+			format!("\nThis config can not be used with: `{}`", v.join("`, `"))
+		} else {
+			"".to_string()
+		};
+
+		let tmp = wrap_all(&format!(
+			"
+			#### `{}`
+
+			* Default: {}
+			* Type: {}
+			{}
+			{}
+			{}
+			{}
+			",
+			self.key,
+			unindent(&self.default_description),
+			unindent(&self.type_description),
+			deprecated,
+			description,
+			exclusive,
+			no_env_export,
+		));
+
+		unindent(&tmp)
 	}
 }
 
@@ -151,11 +211,88 @@ fn describe_usage(
 	}
 }
 
-pub struct DefinitionOptions {
-	key: &'static str,
-	default: TypeDef,
-	types: &'static [TypeDef],
-	description: &'static str,
-	multiple: bool,
-	short: Option<&'static [&'static str]>,
+fn terminal_columns() -> usize {
+	static TERMINAL_COLUMNS: OnceLock<usize> = OnceLock::new();
+
+	*TERMINAL_COLUMNS.get_or_init(|| {
+		if let Some(size) = terminal_size::terminal_size() {
+			return size.0.0 as usize;
+		}
+		80
+	})
+}
+
+fn wrap(s: &str) -> String {
+	let cols = terminal_columns().clamp(20, 80) - 5;
+	let mut ret: Vec<String> = Vec::new();
+	let mut words: Vec<&str> = Vec::new();
+	let mut len = 0;
+	let s = unindent(s);
+
+	for word in s.split_ascii_whitespace() {
+		if len + word.len() > cols {
+			ret.push(words.join(" "));
+			words.clear();
+			len = 0;
+		} else {
+			words.push(word);
+			len += word.len();
+		}
+	}
+
+	if !words.is_empty() {
+		ret.push(words.join(" "));
+	}
+
+	ret.join("\n")
+}
+
+fn wrap_all(s: &str) -> String {
+	let mut in_code_block = false;
+
+	s.split("\n\n")
+		.map(|block| {
+			if in_code_block || block.starts_with("```") {
+				in_code_block = !block.ends_with("```");
+				return block.to_string();
+			}
+			if block.starts_with('*') {
+				let tmp = block
+					.chars()
+					.skip(1)
+					.collect::<String>()
+					.trim_ascii()
+					.split("\n* ")
+					.map(|li| wrap(li).replace("\n", "\n  "))
+					.collect::<Vec<String>>()
+					.join("\n* ");
+				return format!("* {}", tmp);
+			}
+			wrap(block)
+		})
+		.collect::<Vec<String>>()
+		.join("\n\n")
+}
+
+#[cfg(test)]
+mod tests {
+	use super::{Definition, DefinitionOptions, TypeDef};
+
+	#[test]
+	fn basic_definition() {
+		let def = Definition::new(DefinitionOptions {
+			key: "key",
+			default: TypeDef::StringV("some default value"),
+			types: &[TypeDef::Number, TypeDef::String],
+			description: "just a test thingie",
+			multiple: false,
+			short: None,
+			deprecated: None,
+			exclusive: None,
+		});
+		assert_eq!(def.hint, "<key>");
+		assert_eq!(def.usage, "--key <key>");
+		assert_eq!(def.type_description, "Number or String");
+		assert!(def.env_export);
+	}
 }
